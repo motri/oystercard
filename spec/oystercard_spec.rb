dcb00004e43204f3d299bdbbcce2c80eb04949bc
Unai Motriko <unaimotriko@gmail.com>
2017-04-17 16:11:02 +0100

    Step 5 adding to your balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,14 @@ describe Oystercard do
     expect(subject.balance).not_to eq nil
   end
 
+  describe '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'tops up your balance' do
+      expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
+    end
+
+  end
+
 end
